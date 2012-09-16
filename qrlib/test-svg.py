83a782e98bc25bd6b3f444164c1da577092fe2be
Miguel Paolino <mpaolino@gmail.com>
2012-09-16 20:17:47 -0300

    Fixed styles SVGs so it won't show joints (increase stroke with to 0.5, centered in 0,0)

--- qrlib/test-svg.py
+++ qrlib/test-svg.py
@@ -2,15 +2,15 @@ import qrsvg
 import pyqrcode
 
 #url = 'http://ilskdjakldjlkadjlkajsdlkajdlkajsdlkajdlkjaldkjalksdjalkdjaslkdjaslkdjaslkdjaslkdjaslkdjsalkdjsalkdjaslkdjaslkdjaslkdjadeal.com.uy'
-url = 'asadsdasdasd'
+url = 'http://ideal.com.uy'
 
 qrcode = qrsvg.generate_QR_for_url(url, 
-                                   style_color='#CC66CC',
-                                   style='circle',
-                                   inner_eye_style='circle',
-                                   inner_eye_color='green',
-                                   outer_eye_style='stylish',
-                                   outer_eye_color='purple')
+                                   style_color='#193CD4',
+                                   style='default',
+                                   inner_eye_style='default',
+                                   inner_eye_color='#193CD4',
+                                   outer_eye_style='default',
+                                   outer_eye_color='#1466C4')
 f = open('test.svg', 'w')
 f.write(qrcode.getvalue())
 f.close()
